03857e73279d30618c2a28a0436406b741ff3e19
SoseD-11 <kost1991ya@mail.ru>
2022-09-11 14:44:30 +0300

    задание 6 выполнено

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var totalWeight = boxerWeight1 + boxerWeight2;
+        System.out.println("Общий вес двух бойцов " + totalWeight + " кг!");
+    }
+}
